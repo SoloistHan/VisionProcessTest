3ce9fcb9cb71efd4293b17e739438faedee14ae8
Soloist <lineage18135@yahoo.com.tw>
2021-02-01 22:26:43 +0800

    First commit

---
+++ VisionProcessTest/BLL_NewObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisionProcessTest
+{
+    public class BLL_NewObject : Form_Main
+    {
+        public static void NewSome(Control Where)
+        {
+            MenuStrip menuStrip = new MenuStrip();
+            menuStrip.Name = "MenuStrip01";
+            menuStrip.Items.Add("TEST");
+            Where.Controls.Add(menuStrip);
+        }
+    }
+}
